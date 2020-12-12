761c5619abfe2ec4ac93e652862b54cf9b2e3b26
Gaurav Panwar <g-p@live.in>
2020-12-12 20:08:53 +0530

    Add project files.

---
+++ Memories/MainWindow.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports LibVLCSharp.Shared
+
+Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        'p1.Play()
+        'Core.Initialize()
+        'library = New LibVLC()
+
+        'AddHandler p1.Loaded, AddressOf VideoView_Loaded
+    End Sub
+
+    Private Sub VideoView_Loaded(sender As Object, e As RoutedEventArgs)
+        'p1.MediaPlayer = New MediaPlayer(library)
+        'p1.MediaPlayer.Play(New Media(library, New Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"))) ' "D:\Recordings\VID_20200917_213043.mp4"))
+    End Sub
+End Class
